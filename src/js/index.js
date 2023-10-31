d345139d91f00dcf25f7c22b6d0fb467e7120edd
Serhii Siabro <sergey.syabro95@gmail.com>
2023-10-31 22:23:49 +0200

    html work

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,27 @@ const tabsContentHolder = document.getElementsByClassName(
 const dateTimeStart = document.querySelector("date-time-start");
 const dateTimeEnd = document.querySelector("date-time-end");
 
+function durationBetweenDates(
+  startDate = Date.now(),
+  endDate = Date.now(),
+  dimension = "days"
+) {
+  let difference = Math.abs(new Date(endDate) - new Date(startDate));
+
+  switch (dimension) {
+    case "seconds":
+      return `${difference / 1000} ${dimension}`;
+    case "minutes":
+      return `${difference / (1000 * 60)} ${dimension}`;
+    case "hours":
+      return `${difference / (1000 * 60 * 60)} ${dimension}`;
+    case "days":
+      return `${difference / (1000 * 60 * 60 * 24)} ${dimension}`;
+    default:
+      console.log(`Sorry, we cant find ${dimension}`);
+  }
+}
+
 console.log(dateTimeStart);
 const sumFunc = () => {
   console.log("hi");
