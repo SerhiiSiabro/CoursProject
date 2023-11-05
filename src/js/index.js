b05fddc433f2fa7d9d39ea1b31b8e87bdb89dd40
Serhii Siabro <sergey.syabro95@gmail.com>
2023-11-05 21:46:54 +0200

    added: Юзер може обрати пресети проміжків часу

--- src/js/index.js
+++ src/js/index.js
@@ -5,32 +5,42 @@ const tabsContentHolder = document.getElementsByClassName(
   ".tabs-content-holder"
 );
 
-const dateTimeStart = document.querySelector("date-time-start");
-const dateTimeEnd = document.querySelector("date-time-end");
+const dateTimeStart = document.getElementById("date-time-start");
+const dateTimeEnd = document.getElementById("date-time-end");
+const buttonWeek = document.getElementById("button-week");
+const buttonMonth = document.getElementById("button-month");
 
-function durationBetweenDates(
-  startDate = Date.now(),
-  endDate = Date.now(),
-  dimension = "days"
-) {
-  let difference = Math.abs(new Date(endDate) - new Date(startDate));
+function setTodayDate() {
+  const today = new Date();
+  return formatDate(today);
+}
+function formatDate(date) {
+  const year = date.getFullYear();
+  let month = "" + (date.getMonth() + 1);
+  let day = "" + date.getDate();
 
-  switch (dimension) {
-    case "seconds":
-      return `${difference / 1000} ${dimension}`;
-    case "minutes":
-      return `${difference / (1000 * 60)} ${dimension}`;
-    case "hours":
-      return `${difference / (1000 * 60 * 60)} ${dimension}`;
-    case "days":
-      return `${difference / (1000 * 60 * 60 * 24)} ${dimension}`;
-    default:
-      console.log(`Sorry, we cant find ${dimension}`);
+  if (month.length < 2) {
+    month = "0" + month;
   }
+  if (day.length < 2) {
+    day = "0" + day;
+  }
+  return `${year}-${month}-${day}`;
 }
 
-console.log(dateTimeStart);
-const sumFunc = () => {
-  console.log("hi");
-};
-dateTimeStart.addEventListener("input", sumFunc);
+function addWeek() {
+  let dateStart = new Date(dateTimeStart.value);
+  const formatedNewDate = new Date(dateStart.setDate(dateStart.getDate() + 7));
+  dateTimeEnd.value = formatDate(formatedNewDate);
+}
+
+function addMonth() {
+  let dateStart = new Date(dateTimeStart.value);
+  const formatedNewDate = new Date(dateStart.setDate(dateStart.getDate() + 30));
+  dateTimeEnd.value = formatDate(formatedNewDate);
+}
+
+dateTimeStart.value = setTodayDate();
+dateTimeEnd.value = setTodayDate();
+buttonWeek.addEventListener("click", addWeek);
+buttonMonth.addEventListener("click", addMonth);
